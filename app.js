d17bbc84af247cfb7faaf8025b169c14894f0222
FirstBuiltPC <lwenzel90@gmail.com>
2018-11-12 12:37:26 -0500

    formatted all code and added post schema

--- app.js
+++ app.js
@@ -3,38 +3,75 @@
 // is to do with http routing of the app. 
 // Routes point to templated pages using ejs
 // A db is setup using mongo and mongoose below
-var express    = require("express"),
-    app        = express(),
-    logger     = require('./logger.js'),
-    mongoose   = require("mongoose"),
+const express = require("express"),
+    app = express(),
+    logger = require('./logger.js'),
+    mongoose = require("mongoose"),
     bodyParser = require("body-parser");
-    
-    
+
+
 mongoose.connect("mongodb://localhost:27017/reddit_clone", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 
 //body parser seperates out the url info from post requests
 //middleware to use later in app
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Schema Creation
-var userSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+// -----------------------------
+// DB Schema and test insertions
+// -----------------------------
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+const Post = mongoose.model("Post", postSchema);
+
+const newPost = new Post({
+    title: "First Post",
+    content: "Hey world this is a reddit post"
+});
+
+newPost.save(function (err, post) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     email: String,
     isAdmin: Boolean,
-    age: Date, 
+    posts: [postSchema]
 });
 
 // creates a object with methods to manipulate the db
-var User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-// User.create({
-//     username: "JohnsADoeFoSho", 
+// const newUser = new User({
+//     username: "lwenzel90", 
 //     password: "password123", 
 //     email: "example@gmail.com",
-//     isAdmin: true,
-//     age: new Date('January 01, 2000 00:00:00')
+//     isAdmin: true
+// });
+
+// newUser.posts.push(newPost);
+// newUser.save(function(err, user){
+//     if(err){ 
+//         console.log(err) 
+//     } else{
+//         console.log(user);
+//     }
+// });
+
+// User.create({
+//     username: "lwenzel90", 
+//     password: "password123", 
+//     email: "example@gmail.com",
+//     isAdmin: true
 // }, function(err, user){
 //     if(err){
 //         console.log("User entry error");
@@ -45,14 +82,13 @@ var User = mongoose.model("User", userSchema);
 //     }
 // });
 
-var subredditSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true }, 
-    description: {type: String, required: false, unique: false},
-    rules: {type: String, required: false, unique: false}
-    
+const subredditSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: false, unique: false },
+    rules: { type: String, required: false, unique: false }
 });
 
-var Subreddit = mongoose.model("subreddit", subredditSchema);
+const Subreddit = mongoose.model("Subreddit", subredditSchema);
 
 
 // Subreddit.create({
@@ -67,6 +103,9 @@ var Subreddit = mongoose.model("subreddit", subredditSchema);
 //     }
 // });
 
+//-------------
+//Set up routes 
+//-------------
 
 // allow use of nondynamic resources by the app  
 app.use(express.static("public/css"));
@@ -74,61 +113,48 @@ app.use(express.static("public/js"));
 app.use(express.static("public/partials"));
 app.use(express.static("public/img"));
 
-app.get("/", function(req, res){
-    Subreddit.find({}, function(err, subreddits){
-        if(err){
+app.get("/", function (req, res) {
+    Subreddit.find({}, function (err, subreddits) {
+        if (err) {
             console.log("Home page Subreddit query error");
             console.log(err);
-            res.render("Page404", {subreddits: subreddits});
-        } else{
-            res.render("index", {subreddits: subreddits});
+            res.render("Page404", { subreddits: subreddits });
+        } else {
+            res.render("index", { subreddits: subreddits });
         }
     });
 });
 
 //subreddit index route 
-app.get("/subreddit", function(req, res){
-    Subreddit.find({}, function(err, subreddits){
-        if(err){
+app.get("/subreddit", function (req, res) {
+    Subreddit.find({}, function (err, subreddits) {
+        if (err) {
             console.log("Subreddit query error");
             console.log(err);
-            res.render("index", {subreddits: subreddits});
-        } else{
-            res.render("subreddit", {subreddits: subreddits});
+            res.render("index", { subreddits: subreddits });
+        } else {
+            res.render("subreddit", { subreddits: subreddits });
         }
     });
 });
 
 // route to form to make new subreddit 
-app.get("/subreddit/new", function(req, res){
+app.get("/subreddit/new", function (req, res) {
     res.render("newSubreddit");
 });
 
-// Show an individual subreddit
-app.get("/subreddit/:id", function(req, res){
-    Subreddit.findById(req.params.id, function(err, foundSubreddit){
-        if(err){
-            console.log(err);
-        } else{
-            //render show template
-            res.render("subredditShow", {subreddit: foundSubreddit });
-            
-        }
-    });
-});
-
 // adds subreddit to database from form
-app.post("/subreddit", function(req, res){
-    var name = req.body.name;
-    var description = req.body.description;
-    var rules = req.body.rules;
-    var newSubreddit = {name: name, description: description, rules: rules};
-    
-    Subreddit.create(newSubreddit, function(err, newEntry){
-        if(err){
+app.post("/subreddit", function (req, res) {
+    const name = req.body.name;
+    const description = req.body.description;
+    const rules = req.body.rules;
+    const newSubreddit = { name: name, description: description, rules: rules };
+
+    Subreddit.create(newSubreddit, function (err, newEntry) {
+        if (err) {
             console.log("subreddit creation error");
             console.log(err);
-        } else{
+        } else {
             console.log("Below subreddit has been added to DB");
             console.log(newEntry.name);
             res.redirect("/subreddit");
@@ -136,41 +162,51 @@ app.post("/subreddit", function(req, res){
     });
 });
 
+// Show an individual subreddit
+app.get("/subreddit/:id", function (req, res) {
+    Subreddit.findById(req.params.id, function (err, foundSubreddit) {
+        if (err) {
+            console.log(err);
+        } else {
+            //render show template
+            res.render("subredditShow", { subreddit: foundSubreddit });
+        }
+    });
+});
 
-
-app.get("/users", function(req, res){
-    User.find({}, function(err, allUsers){
-        if(err){
+app.get("/users", function (req, res) {
+    User.find({}, function (err, allUsers) {
+        if (err) {
             console.log("Users query error");
             console.log(err.message);
-        } else{
-            res.render("users", {users:allUsers});
+        } else {
+            res.render("users", { users: allUsers });
         }
     })
 });
 
-app.get("/posts", function(req, res){
-    var posts = [
-        {title: "First post on reddit", author: "Mike"},
-        {title: "Look at this meme", author: "Tyler"},
-        {title: "Classic Post!", author: "Rogger"}
-        ];
-    res.render("posts", {posts: posts});
+app.get("/posts", function (req, res) {
+    const posts = [
+        { title: "First post on reddit", author: "Mike" },
+        { title: "Look at this meme", author: "Tyler" },
+        { title: "Classic Post!", author: "Rogger" }
+    ];
+    res.render("posts", { posts: posts });
 });
 
-app.get("/:pageName", function(req, res){ 
-    var pageName = req.params.pageName;
-    console.log(pageName.name);
-    res.render("page404", {pageVar: pageName});
+app.get("/:pageName", function (req, res) {
+    const pageName = req.params.pageName;
+    console.log(req.originalUrl);
+    res.render("page404", { pageVar: pageName });
 });
 
 //show subreddits
-app.get("/r/:subreddit", function(req, res){
-    var subreddit = req.params.subreddit;
+app.get("/r/:subreddit", function (req, res) {
+    const subreddit = req.params.subreddit;
     res.send("You are trying to connect to the '" + subreddit + "' the subreddit");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Listening on port 3000");
 });
 
