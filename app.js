5163d6944a7e0780f5e7b4a5d704b2d4dbd0ca57
FirstBuiltPC <lwenzel90@gmail.com>
2018-11-28 09:20:56 -0500

    Now listens on port 80

--- app.js
+++ app.js
@@ -232,4 +232,4 @@ app.get("/user", function (req, res) {
 //     res.render("page404", { pageVar: pageName });
 // });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(80, () => console.log("Listening on port default port(80)"));
