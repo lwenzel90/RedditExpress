20a5db3fd2458f50d2356a62034a9c5f202289fd
FirstBuiltPC <lwenzel90@gmail.com>
2018-11-28 08:47:20 -0500

    Compose button will disappear if not logged in. Navbar added to all subreddit pages that are not the form

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const express = require("express"),
     seedDB = require("./seeds"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
+    path = require("path"),
     passportLocalMongoose = require("passport-local-mongoose");
 
 
@@ -43,8 +44,8 @@ passport.deserializeUser(User.deserializeUser());
 app.use(express.static("public/css"));
 app.use(express.static("public/js"));
 app.use(express.static("public/partials"));
-app.use(express.static("public/img"));
-
+app.use(express.static(__dirname + "/public/img"));
+console.log(__dirname);
 //-------------
 //Set up routes 
 //-------------
@@ -60,16 +61,12 @@ app.get("/", function (req, res) {
                 user: req.user,
                 subreddits: subreddits
             }
-            if(req.user){
-                res.render("landing", { data: data });
-            } else {
-                res.render("landing", {})
-            }
+            res.render("landing", { data: data });
         }
     });
 });
 
-app.get("/posts/new", isLoggedIn, function(req, res){
+app.get("/posts/new", isLoggedIn, function (req, res) {
     Subreddit.find({}, function (err, subreddits) {
         if (err) {
             console.log("Home page Subreddit query error");
@@ -81,8 +78,8 @@ app.get("/posts/new", isLoggedIn, function(req, res){
     });
 });
 
-app.post("/posts", isLoggedIn, function(req, res){
-    if(req.user){
+app.post("/posts", isLoggedIn, function (req, res) {
+    if (req.user) {
         const title = req.body.title;
         const content = req.body.content;
         const user = req.user.username;
@@ -90,47 +87,47 @@ app.post("/posts", isLoggedIn, function(req, res){
         // NEED TO DO 
         // find the subreddit that the form returns then add the post to the subreddit 
         // add post to the subreddit and add the subreddit to the post
-        const newPost = { title: title, content: content, user: user, subreddit: subreddit};
-    
-    } else{
+        const newPost = { title: title, content: content, user: user, subreddit: subreddit };
+
+    } else {
         res.send("This is not working");
     }
 });
 
 //user sign up form 
-app.get("/user/register", function(req, res){
+app.get("/user/register", function (req, res) {
     res.render("users/register");
 });
 
 //handle form submit of new user
-app.post("/register", function(req, res){
+app.post("/register", function (req, res) {
     //hashes password
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-        if(err){
+    User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             return res.render("users/register");
         }
         //log user in and store info 
-        passport.authenticate("local")(req, res, function(){
+        passport.authenticate("local")(req, res, function () {
             res.send("you have been authenticated and logged in");
         });
     });
 });
 
 app.post("/login", passport.authenticate("local", {
-    successRedirect: "/", 
+    successRedirect: "/",
     failureRedirect: "/failedLogin"
-}) ,function(req,res){
+}), function (req, res) {
 
 });
 
-app.get("/logout", function(req, res){
+app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
 });
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/");
@@ -144,7 +141,13 @@ app.get("/subreddit", function (req, res) {
             console.log(err);
             res.render("landing", { subreddits: subreddits });
         } else {
-            res.render("subreddits/show", { subreddits: subreddits });
+
+            let data = {
+                user: req.user,
+                subreddits: subreddits
+            }
+            res.render("subreddits/show", { data: data });
+
         }
     });
 });
@@ -169,29 +172,47 @@ app.post("/subreddit", function (req, res) {
             console.log("Below subreddit has been added to DB");
             console.log(newEntry.name);
             Subreddit.find({}, function (err, subreddits) {
-                if(err){
+                if (err) {
                     console.log(err);
                 } else {
                     res.render("subreddits/show", { subreddits: subreddits });
                 }
-                
+
             });
-            
+
         }
     });
 });
 
 // subreddit index route (individual subreddit)
+// gets all subreddits for nav bar then gets the specific 
+// subreddit by the id
 app.get("/subreddit/:id", function (req, res) {
+    let allSubreddits;
+    Subreddit.find({}, function (err, subreddits){
+        if(err){
+            console.log(err);
+            res.redirect("/");
+        } else{
+            allSubreddits = subreddits;
+        }
+    });
+    
     Subreddit.findById(req.params.id).populate("posts").exec(function (err, foundSubreddit) {
         if (err) {
             console.log(err);
         } else {
+            let data = {
+                foundSubreddit: foundSubreddit,
+                allSubreddits: allSubreddits, 
+                user: req.user
+            }
             console.log(`found subreddit ${foundSubreddit}`);
             //render show template
-            res.render("subreddits/index", { subreddit: foundSubreddit });
+            res.render("subreddits/index", { data: data });
         }
     });
+    
 });
 
 app.get("/user", function (req, res) {
